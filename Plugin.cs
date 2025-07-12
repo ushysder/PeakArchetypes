607a2f0c9cb67e3dbdf0fda1a839beec8e82e873
zeeblo <141220959+zeeblo@users.noreply.github.com>
2025-07-12 11:19:21 -0700

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+
+namespace KomiChallenge
+{
+
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string modGUID = "KomiChallenge.zeeblo.dev";
+        public const string modName = "KomiChallenge";
+        public const string modVersion = "0.1.0";
+        private readonly Harmony _harmony = new(modGUID);
+        internal static ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+        public static ulong localID;
+
+        void Awake()
+        {
+            PatchAllStuff();
+
+        }
+
+        public static void ResetAllVariables()
+        {
+
+        }
+
+
+        private void PatchAllStuff()
+        {
+            _harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public static void SendLog(string msg)
+        {
+            mls.LogInfo(msg);
+        }
+    }
+}
---
+++ Utils/ztests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace KomiChallenge.Utils
+{
+    [HarmonyPatch]
+    internal class ztests
+    {
+
+        [HarmonyPatch(typeof(MountainProgressHandler), "Update")]
+        [HarmonyPostfix]
+        private static void boopPatch(MountainProgressHandler __instance)
+        {
+            if (Keyboard.current.digit1Key.wasPressedThisFrame)
+            {
+                GUIManager.instance.SetHeroTitle("boop!", __instance.progressPoints[0].clip);
+            }
+        }
+
+    }
+}
